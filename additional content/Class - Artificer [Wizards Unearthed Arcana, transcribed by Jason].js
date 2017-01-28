80d4b458eb8eea6ad9affa82a1dc2f14ea68e1a1
Jason Rawlins <jasonrawlins@hotmail.com>
2017-01-27 18:29:06 -0800

    Changed intances of "you" to "I" and fixed other pronouns as appropriate.

--- additional content/Class - Artificer [Wizards Unearthed Arcana, transcribed by Jason].js
+++ additional content/Class - Artificer [Wizards Unearthed Arcana, transcribed by Jason].js
@@ -46,7 +46,7 @@ ClassList["artificer"] = {
     die: 8,
     saves: ["Con", "Int"],
     skills: ["\n\n" + toUni("Artificer") + ": Choose three from Arcana, Deception, History, Investigation, Medicine, Nature, Religion, Sleight of Hand"],
-    tools: ["Thieves' tools and two others of your choice"],
+    tools: ["Thieves' tools and two others of my choice"],
     armor: [
         [true, true, false, false]
     ],
@@ -116,13 +116,13 @@ ClassList["artificer"] = {
             name: "Mechanical Servant",
             source: ["UA:A", 4],
             minlevel: 6,
-            description: "\n   Through research and mastery of my craft, I have producted a mechanical servant. It obeys my commands and protects me in combat. The servant is not magical. Select a Large beast with a CR of 2 or less. The servant uses that beast's game statistics but can look however you like as long as its form is appropriate for its statistics.\n   It has the following modifications:\n    \u2022 It is a construct instead of a beast.\n    \u2022 It can't be charmed.\n    \u2022 It is immune to poison and the poisoned condition.\n    \u2022 It gains darkvision with a range of 60 feet if it doesn't have it already.\n    \u2022 It understands the languages you can speak when you create it, but it can't speak.\n    \u2022 If you are the target of a melee attack and the servant is within 5 feet of the attacker, you can use your reaction to command the servant to respond, using its reaction to make a melee attack against the attacker.\n   In combat, it rolls its own initiative and acts on its own. If the servant is killed, it can be returned to life via normal means. You can repair your slain servant during a long rest if you have access to its body. It returns to life with 1 HP after the rest. If it is beyond recovery, you can create a new one with one week of work and 1,000 GP."
+            description: "\n   Through research and mastery of my craft, I have producted a mechanical servant. It obeys my commands and protects me in combat. The servant is not magical. Select a Large beast with a CR of 2 or less. The servant uses that beast's game statistics but can look however I like as long as its form is appropriate for its statistics.\n   It has the following modifications:\n    \u2022 It is a construct instead of a beast.\n    \u2022 It can't be charmed.\n    \u2022 It is immune to poison and the poisoned condition.\n    \u2022 It gains darkvision with a range of 60 feet if it doesn't have it already.\n    \u2022 It understands the languages I can speak when I create it, but it can't speak.\n    \u2022 If I am the target of a melee attack and the servant is within 5 feet of the attacker, I can use my reaction to command the servant to respond, using its reaction to make a melee attack against the attacker.\n   In combat, it rolls its own initiative and acts on its own. If the servant is killed, it can be returned to life via normal means. I can repair my slain servant during a long rest if I have access to its body. It returns to life with 1 HP after the rest. If it is beyond recovery, I can create a new one with one week of work and 1,000 GP."
         },
         "soul of artifice": {
             name: "Soul of Artifice",
             source: ["UA:A", 1],
             minlevel: 20,
-            description: "\n   You can attune to up to six magic items at once. In addition, you gain a +1 bonus to all saving throws per magic item you are attuned to."
+            description: "\n   I can attune to up to six magic items at once. In addition, I gain a +1 bonus to all saving throws per magic item I am attuned to."
         },
         "subclassfeature1": {
             name: "Specialist",
@@ -143,7 +143,7 @@ ClassSubList["alchemist"] = {
             name: "Alchemist's Satchel",
             source: ["UA:A", 5],
             minLevel: 1,
-            description: "You have crafted a magical Alchemist's Satchel. You always pull out exactly the right materials for your formulas. If you lose the satchel, you can create a new one of the course of three days work and spending 100g."
+            description: "I have crafted a magical Alchemist's Satchel. I always pull out exactly the right materials for my formulas. If I lose the satchel, I can create a new one of the course of three days work and spending 100g."
         },
         "alchemical formula": {
             name: "Alchemical Formula",
@@ -156,37 +156,37 @@ ClassSubList["alchemist"] = {
             "alchemical fire": {
                 name: "Alchemical Fire",
                 source: ["UA:A", 5],
-                description: "\n   As an action, you can reach into your Alchemist’s Satchel, pull out a vial of volatile liquid, and hurl the vial at a creature, object, or surface within 30 feet of you (the vial and its contents disappear if you don’t hurl the vial by the end of the current turn). On impact, the vial detonates in a 5-foot radius. Any creature in that area must succeed on a Dexterity saving throw or take 1d6 fire damage. This formula’s damage increases by 1d6 when you reach certain levels in this class: 4th level (2d6), 7th level (3d6), 10th level (4d6), 13th level (5d6), 16th level (6d6), and 19th level (7d6)."
+                description: "\n   As an action, I can reach into my Alchemist’s Satchel, pull out a vial of volatile liquid, and hurl the vial at a creature, object, or surface within 30 feet of me (the vial and its contents disappear if I don’t hurl the vial by the end of the current turn). On impact, the vial detonates in a 5-foot radius. Any creature in that area must succeed on a Dexterity saving throw or take 1d6 fire damage. This formula’s damage increases by 1d6 when I reach certain levels in this class: 4th level (2d6), 7th level (3d6), 10th level (4d6), 13th level (5d6), 16th level (6d6), and 19th level (7d6)."
             },
             "alchemical acid": {
                 name: "Alchemical Acid",
                 source: ["UA:A", 5],
-                description: "\n   As an action, you can reach into your Alchemist’s Satchel, pull out a vial of acid, and hurl the vial at a creature or object within 30 feet of you (the vial and its contents disappear if you don’t hurl the vial by he end of the current turn). The vial shatters on impact. A creature must succeed on a Dexterity saving throw or take 1d6 acid damage. An object automatically takes that damage, and the damage is maximized. This formula’s damage increases by 1d6 when you reach certain levels in this class: 3rd level (2d6), 5th level (3d6), 7th level (4d6), 9th level (5d6), 11th level (6d6), 13th level (7d6), 15th level (8d6), 17th level (9d6), and 19th level (10d6)."
+                description: "\n   As an action, I can reach into my Alchemist’s Satchel, pull out a vial of acid, and hurl the vial at a creature or object within 30 feet of me (the vial and its contents disappear if I don’t hurl the vial by he end of the current turn). The vial shatters on impact. A creature must succeed on a Dexterity saving throw or take 1d6 acid damage. An object automatically takes that damage, and the damage is maximized. This formula’s damage increases by 1d6 when I reach certain levels in this class: 3rd level (2d6), 5th level (3d6), 7th level (4d6), 9th level (5d6), 11th level (6d6), 13th level (7d6), 15th level (8d6), 17th level (9d6), and 19th level (10d6)."
             },
             "healing draught": {
                 name: "Healing Draught",
                 source: ["UA:A", 5],
-                description: "\n   As an action, you can reach into your Alchemist’s Satchel and pull out a vial of healing liquid. A creature can drink it as an action to regain 1d8 hit points. The vial then disappears. Once a creature regains hit points from this alchemical formula, the creature can’t do so again until it finishes a long rest. If not used, the vial and its contents disappear after 1 hour. While the vial exists, you can’t use this formula. This formula’s healing increases by 1d8 when you reach certain levels in this class: 3rd level (2d8), 5th level (3d8), 7th level (4d8), 9th level (5d8), 11th level (6d8), 13th level (7d8), 15th level (8d8), 17th level (9d8), and 19th level (10d8)."
+                description: "\n   As an action, I can reach into my Alchemist’s Satchel and pull out a vial of healing liquid. A creature can drink it as an action to regain 1d8 hit points. The vial then disappears. Once a creature regains hit points from this alchemical formula, the creature can’t do so again until it finishes a long rest. If not used, the vial and its contents disappear after 1 hour. While the vial exists, I can’t use this formula. This formula’s healing increases by 1d8 when I reach certain levels in this class: 3rd level (2d8), 5th level (3d8), 7th level (4d8), 9th level (5d8), 11th level (6d8), 13th level (7d8), 15th level (8d8), 17th level (9d8), and 19th level (10d8)."
             },
              "smoke stick": {
                 name: "Smoke Stick",
                 source: ["UA:A", 5],
-                description: "\n   As an action, you can reach into your Alchemist’s Satchel and pull out a stick that produces a thick plume of smoke. You can hold on to the stick or throw it to a point up to 30 feet away as part of the action used to produce it. The area in a 10-foot radius around the stick is filled with thick smoke that blocks vision, including darkvision. The stick and smoke persist for 1 minute and then disappear. After using this formula, you can’t do so again for 1 minute."
+                description: "\n   As an action, I can reach into my Alchemist’s Satchel and pull out a stick that produces a thick plume of smoke. I can hold on to the stick or throw it to a point up to 30 feet away as part of the action used to produce it. The area in a 10-foot radius around the stick is filled with thick smoke that blocks vision, including darkvision. The stick and smoke persist for 1 minute and then disappear. After using this formula, I can’t do so again for 1 minute."
             },
              "swift step draught": {
                 name: "Swift Step Draught",
                 source: ["UA:A", 5],
-                description: "\n   As a bonus action, you can reach into your Alchemist’s Satchel and pull out a vial filled with a bubbling, brown liquid. As an action, a creature can drink it. Doing so increases the creature’s speed by 20 feet for 1 minute, and the vial disappears. If not used, the vial and its contents disappear after 1 minute. After using this formula, you can’t do so again for 1 minute."
+                description: "\n   As a bonus action, I can reach into my Alchemist’s Satchel and pull out a vial filled with a bubbling, brown liquid. As an action, a creature can drink it. Doing so increases the creature’s speed by 20 feet for 1 minute, and the vial disappears. If not used, the vial and its contents disappear after 1 minute. After using this formula, I can’t do so again for 1 minute."
             },
             "tanglefoot bag": {
                 name: "Tanglefoot Bag",
                 source: ["UA:A", 6],
-                description: "\n   As an action, you can reach into your Alchemist’s Satchel and pull out a bag filled with writhing, sticky black tar and hurl it at a point on the ground within 30 feet of you (the bag and its contents disappear if you don’t hurl the bag by the end of the current turn). The bag bursts on impact and covers the ground in a 5-foot radius with sticky goo. That area becomes difficult terrain for 1 minute, and any creature that starts its turn on the ground in that area has its speed halved for that turn. After using this formula, you can’t do so again for 1 minute."
+                description: "\n   As an action, I can reach into my Alchemist’s Satchel and pull out a bag filled with writhing, sticky black tar and hurl it at a point on the ground within 30 feet of me (the bag and its contents disappear if I don’t hurl the bag by the end of the current turn). The bag bursts on impact and covers the ground in a 5-foot radius with sticky goo. That area becomes difficult terrain for 1 minute, and any creature that starts its turn on the ground in that area has its speed halved for that turn. After using this formula, I can’t do so again for 1 minute."
             },
             "thunderstone": {
                 name: "Thunderstone",
                 source: ["UA:A", 6],
-                description: "\n   As an action, you can reach into your Alchemist’s Satchel and pull out a crystalline shard and hurl it at a creature, object, or surface within 30 feet of you (the shard disappears if you don’t hurl it by the end of the current turn). The shard shatters on impact with a blast of concussive energy. Each creature within 10 feet of the point of impact must succeed on a Constitution saving throw or be knocked prone and pushed 10 feet away from that point."
+                description: "\n   As an action, I can reach into my Alchemist’s Satchel and pull out a crystalline shard and hurl it at a creature, object, or surface within 30 feet of me (the shard disappears if I don’t hurl it by the end of the current turn). The shard shatters on impact with a blast of concussive energy. Each creature within 10 feet of the point of impact must succeed on a Constitution saving throw or be knocked prone and pushed 10 feet away from that point."
             }
         }
     }
